af59129497ae1f88ed5a9165091ee6be5e32746a
BerezentesvEvgeny <jonn09@icloud.com>
2022-05-30 08:43:46 +0300

    Initial Commit

---
+++ WhatsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsApp
+//
+//  Created by Евгений Березенцев on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
